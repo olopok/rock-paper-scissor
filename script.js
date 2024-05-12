b674e4fb7bd24b8512b3b291a88a1bb9e192554d
olopok <pabisa.pb@gmail.com>
2024-05-12 14:38:12 +0100

    create index.html and script.js and add computer choice func.

---
+++ script.js
@@ -0,0 +1,36 @@
+// DECLARE variable to store computer choice.
+
+let computerChoice = '';
+getComputerChoice();
+
+// DECLARE the function for computer choice.
+function getComputerChoice() {
+
+    // DECLARE variable for 'rock'.
+    const rock = 'rock';
+
+    // DECLARE variable for 'paper'.
+    const paper = 'paper';
+
+    // DECLARE variable for 'scissor'.
+    const scissor = 'scissor';
+
+    // DECLARE variable to store the random value and GET the value.
+
+    let randomValue = Math.floor(Math.random() * 3) + 1;
+
+    // IF condition for 1.
+    if (randomValue === 1) {
+        computerChoice = rock;
+    }
+
+    // ELSE IF condition for 2.
+    else if (randomValue === 2) {
+        computerChoice = paper;
+    }
+
+    // ELSE condition for 3.
+    else {
+        computerChoice = scissor
+    }
+}
