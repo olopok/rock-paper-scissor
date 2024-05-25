0068448b0bb6f8dffc8bf4c93193a66342db1b9d
olopok <pabisa.pb@gmail.com>
2024-05-25 21:33:01 +0100

    Add a simple UI to the game

--- script.js
+++ script.js
@@ -1,95 +1,124 @@
-let computerChoice = '';
-let humanChoice = '';
-
-playGame();
-
-function getComputerChoice() {
-
-    let randomValue = Math.floor(Math.random() * 3) + 1;
-
-    if (randomValue === 1) {
-        computerChoice = 'rock';
-    } else if (randomValue === 2) {
-        computerChoice = 'paper';
-    } else {
-        computerChoice = 'scissor';
-    }
+const BTNS = document.querySelectorAll('button');
+for (const btn of BTNS) {
+    btn.addEventListener("click", playGame);
 }
 
-function getHumanChoice() {
-    const HUMAN_INPUT = prompt('Write your choice');
-    humanChoice = HUMAN_INPUT;
-}
+let div = document.createElement('div');
+const body = document.querySelector("body");
+body.appendChild(div);
+let p = document.createElement("p");
+let p1 = document.createElement('p');
+let p2 = document.createElement('p');
 
-humanChoice = humanChoice.toLowerCase();
+let humanScore = 0;
+let computerScore = 0;
+let winnerMessage = '';
+let roundNumber = 0;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function playGame(e) {
+    const humanChoice = e.target.id;
+    console.log(humanChoice);
 
-    for (let i = 0; i < 5; i++) {
-        getComputerChoice();
-        getHumanChoice();
-        playRound(humanChoice, computerChoice);
+    let computerChoice = '';
+
+    computerChoice = getComputerChoice();
+
+    function getComputerChoice() {
+
+        let randomValue = Math.floor(Math.random() * 3) + 1;
+
+        if (randomValue === 1) {
+            computerChoice = 'rock';
+        } else if (randomValue === 2) {
+            computerChoice = 'paper';
+        } else {
+            computerChoice = 'scissor';
+        }
+        return computerChoice;
     }
+    console.log(computerChoice);
 
-    function playRound(humanSelection, computerSelection) {
 
-        switch (humanSelection) {
+    compareChoice(humanChoice, computerChoice);
+
+    function compareChoice(human, computer) {
+        switch (human) {
             case 'rock': {
-                if (computerSelection === 'paper') {
-                    console.log('you lose');
-                    computerScore += 1;
-                } else if (computerSelection === 'scissor') {
-                    console.log('you win');
-                    humanScore += 1;
-                } else {
-                    console.log('The round is par');
-                }
+                roundNumber++;
+
+                if (computer === 'paper') {
+                    // console.log('you lose');
+                    winnerMessage = "Round " + roundNumber + " winner is computer";
+                    computerScore++;
+                } else if (computer === 'scissor') {
+                    // console.log('you win');
+                    winnerMessage = "Round " + roundNumber + " winner is human";
+                    humanScore++;
+                    break;
+                } else { winnerMessage = "Round " + roundNumber + " is par"; }
                 break;
             }
 
             case 'paper': {
-                if (computerSelection === 'rock') {
-                    console.log('you win');
-                    humanScore += 1;
-                } else if (computerSelection === 'scissor') {
-                    console.log('you lose');
-                    computerScore += 1;
-                } else {
-                    console.log('The round is par');
+                roundNumber++;
+                if (computer === 'rock') {
+                    // console.log('you win');
+                    winnerMessage = "Round " + roundNumber + " winner is human";
+                    humanScore++;
+                } else if (computer === 'scissor') {
+                    // console.log('you lose');
+                    winnerMessage = "Round " + roundNumber + " winner is computer";
+                    computerScore++;
                 }
+                else { winnerMessage = "Round " + roundNumber + " is par"; }
+
                 break;
             }
 
             case 'scissor': {
-                if (computerSelection === 'rock') {
-                    console.log('You lose');
+                roundNumber++;
+                if (computer === 'rock') {
+                    // console.log('You lose');
+                    winnerMessage = "Round " + roundNumber + " winner is computer";
                     computerScore += 1;
-                } else if (computerSelection === 'paper') {
-                    console.log('You win');
+                } else if (computer === 'paper') {
+                    // console.log('You win');
+                    winnerMessage = "Round " + roundNumber + " winner is human";
                     humanScore += 1;
-                } else {
-                    console.log('The round is par');
                 }
+                else { winnerMessage = "Round " + roundNumber + " is par"; }
+
                 break;
             }
-
-            default: {
-                console.log('Invalid choice');
-            }
         }
-        
-        console.log('human ', humanScore);
-        console.log('computer ', computerScore);
 
+        console.log(roundNumber);
+        console.log(humanScore, computerScore);
     }
 
-    if (humanScore > computerScore) {
-        alert('The winner is: Human');
-    } else if (humanScore < computerScore) {
-        alert('The winner is: Computer');
-    } else {
-        alert('The game is par');
+    if (humanScore === 5 || computerScore ===5) winnerGame();
+    else displayWinner();
+
+    function winnerGame() {
+        if (humanScore > computerScore) {
+            p.textContent = "THE WINNER IS: HUMAN!!";
+            p1.textContent = "Human score: " + humanScore;
+            p2.textContent = "Computer score: " + computerScore;
+        } else {
+            p.textContent = "THE WINNER IS: COMPUTER!!";
+            p1.textContent = "Human score: " + humanScore;
+            p2.textContent = "Computer score: " + computerScore;
+        }
+    }
+    
+    function displayWinner() {
+        
+            p.textContent = winnerMessage;
+            p1.textContent = "Human score: " + humanScore;
+            p2.textContent = "Computer score: " + computerScore;
+            div.appendChild(p);
+            div.appendChild(p1);
+            div.appendChild(p2);
+        
     }
 }
